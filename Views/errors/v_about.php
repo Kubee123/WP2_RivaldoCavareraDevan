e911ba6b8c2220bbee99c7dc0f55de2689257297
Kubee123 <19220999@bsi.ac.id>
2023-10-26 20:14:14 +0700

    first commit

---
+++ Views/errors/v_about.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <title>Halaman About</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <link rel='stylesheet' type='text/css' href='https://localhost/mocca-be/assets/css/stylebuku.css'>
+    </head>
+    <body>
+        <section>
+            <h1><?php echo $judul ?></h1>
+            <h4>Nama</h4>
+            <ul type="disc">
+                <li>Nama Depan : Rivaldo Cavarera</li>
+                <li>Nama Belakang : Devan</li>
+            </ul>
+            <br>
+            <h4>Alamat</h4>
+            <ul type="none">
+                <li>Jalan. Warakas 1 Gg.A No.49A RT:012/RW:003 Warakas</li>
+            </ul>
+            <h4>Tempat Lahir</h4>
+            <ul type="none">
+                <li>Jakarta</li>
+            </ul>
+            <h4>Olah Raga Favorit</h4>
+            <ul type="Square">
+                <li>Futsal</li>
+                <li>Bulutangkis</li>
+        </section>
+    </body>
+</html>
---
+++ Views/errors/v_footer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Rental Buku</title>
+    </head>
+    <body>
+        <div>
+            <footer>
+                <a href="https://RentalBuku.com">Rivaldo Cavarera Devan</a>
+            </footer>
+        </div>
+    </body>
+</html>
